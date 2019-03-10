1017ba445cdf8f9a2bda4e658d875e86e0040f5d
David <DavidFricker@users.noreply.github.com>
2019-03-10 17:35:34 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ sudo apt-get update
 sudo apt-get install apache2-utils squid -y
 sudo apt-get install privoxy
 
-
+#config commands
 read -e -p "Your desired username: " usrn
 htpasswd -c /etc/squid/passwd $usrn
 touch /etc/squid/blacklist.acl	
-
 wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/DavidFricker/squid-basic-auth-privoxy/master/privoxyconfig.conf
 
+#run
 service squid restart && update-rc.d squid defaults
 firew2
