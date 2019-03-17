c8ac9dbfed9caac3b7947c6a2ccebb9b96d3e51d
David <DavidFricker@users.noreply.github.com>
2019-03-17 16:41:21 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,19 +7,22 @@ sudo apt-get install privoxy
 #config commands
 read -e -p "Your desired username: " usrn
 htpasswd -c /etc/squid/passwd $usrn
-touch /etc/squid/blacklist.acl	
+touch /etc/squid/blacklist.acl
 wget -O /etc/squid/squid.conf https://raw.githubusercontent.com/DavidFricker/squid-basic-auth-privoxy/master/privoxyconfig.conf
 wget -O /etc/squid/squidpt.conf https://raw.githubusercontent.com/DavidFricker/squid-basic-auth-privoxy/master/privoxyconfigpassthrough.conf
 wget -O /etc/init.d/squidpt https://raw.githubusercontent.com/DavidFricker/squid-basic-auth-privoxy/master/initdpassthrough
 
-squid -z -f /etc/squid/squid.conf > /dev/null
-squid -z -f /etc/squid/squidpt.conf > /dev/null
+sudo squid -z -f /etc/squid/squid.conf > /dev/null
+sudo squid -z -f /etc/squid/squidpt.conf > /dev/null
+
+squid -f /etc/squid/squid.conf
+squid -f /etc/squid/squidpt.conf
 
 #run
 #service squid restart && update-rc.d squid defaults
 #service squidpt restart && update-rc.d squidpt defaults
 
-update-rc.d squidpt defaults
+sudo update-rc.d squidpt defaults
 
 # Opening Squid port 8888 for clients to connect
 iptables -I INPUT -p tcp --dport 8888 -j ACCEPT
